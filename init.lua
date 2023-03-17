6b2a952f56edb74d8b50f813cec99ac5e574847e
Jurica Bacurin <jurica@bacurin.de>
2023-03-17 15:33:20 +0100

    - fix Neotree autostart

--- init.lua
+++ init.lua
@@ -493,9 +493,7 @@ cmp.setup {
   },
 }
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  command = "Neotree toggle",
-})
+vim.cmd('Neotree')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
