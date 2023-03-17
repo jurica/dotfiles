c41cedb5bf41ecccab2395740d6fa9e9231d8c65
Jurica Bacurin <jurica@bacurin.de>
2023-03-17 18:28:26 +0100

    - improved script

--- setup.sh
+++ setup.sh
@@ -5,19 +5,23 @@ echo '* setup up dotfiles'
 echo ' ** fetch git submodules'
 git submodule init
 git submodule update
+git -C kickstart.nvim checkout jurica
 
 echo ' ** setup zellij config'
 mkdir -p ~/.config/zellij
-ln -s -f $(realpath zellij/config.kdl) ~/.config/zellij/config.kdl
+rm -f ~/.config/zellij/config.kdl
+ln -s $(realpath zellij/config.kdl) ~/.config/zellij/config.kdl
 
 echo ' ** setup alacritty config'
 mkdir -p ~/.config/alacritty
-ln -s -f $(realpath alacritty/alacritty.yml) ~/.config/alacritty/alacritty.yml
-ln -s -f $(realpath alacritty-theme/themes) ~/.config/alacritty/themes
+rm -f ~/.config/alacritty/alacritty.yml
+rm -f ~/.config/alacritty/themes
+ln -s $(realpath alacritty/alacritty.yml) ~/.config/alacritty/alacritty.yml
+ln -s $(realpath alacritty-theme/themes) ~/.config/alacritty/themes
 
 echo ' ** setup nvim config'
-git -C kickstart.nvim checkout jurica
-ln -s -f $(realpath kickstart.nvim) ~/.config/nvim
+rm -f ~/.config/nvim
+ln -s $(realpath kickstart.nvim) ~/.config/nvim
 
 echo '* finished setup of dotfiles'
 
