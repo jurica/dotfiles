71b3a1650cfd16d4c38e29b948954d0028bec934
Jurica Bacurin <jurica@bacurin.de>
2023-03-19 15:04:58 +0100

    show git blame in lualine

--- lua/custom/plugins/git-blame.lua
+++ lua/custom/plugins/git-blame.lua
@@ -1,3 +1,16 @@
 return {
-  "f-person/git-blame.nvim"
+  "f-person/git-blame.nvim",
+  config = function ()
+    vim.g.gitblame_date_format = '%d.%m.%y %H:%M'
+    vim.g.gitblame_message_template = '<author> • <date> • <summary>'
+    vim.g.gitblame_display_virtual_text = 0
+    local git_blame = require('gitblame')
+    require('lualine').setup({
+      sections = {
+        lualine_c = {
+          { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
+        }
+      }
+    })
+  end
 }
