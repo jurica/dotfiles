a21c12f53163af7eeab4286672742729a93b8471
Jurica Bacurin <jurica@bacurin.de>
2023-03-25 23:23:00 +0100

    new keybinding

--- lua/custom/plugins/zk-nvim.lua
+++ lua/custom/plugins/zk-nvim.lua
@@ -1,4 +1,5 @@
 vim.keymap.set('n', '<leader>zkn', "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", { desc = '[zk] [N]ew Note' })
+vim.keymap.set('n', '<leader>zkj', "<Cmd>ZkNew { group = 'journal', dir = 'journal' }<CR>", { desc = '[zk] Open todays [J]ournal' })
 vim.keymap.set('n', '<leader>zks', "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>", { desc = '[zk] [S]earch Notes' })
 vim.keymap.set('n', '<leader>zka', "<Cmd>ZkNote<CR>", { desc = '[zk] [A]ll Notes' })
 vim.keymap.set('n', '<leader>zkt', "<Cmd>ZkTag<CR>", { desc = '[zk] [T]ags' })
