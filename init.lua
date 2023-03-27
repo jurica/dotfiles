01bbea04f39b5f1d1b336a74bbaf00217b97c8dc
Jurica Bacurin <jurica.bacurin@pascom.net>
2023-03-27 20:03:57 +0200

    show source of diagnostic messages

--- init.lua
+++ init.lua
@@ -365,6 +365,13 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
+vim.diagnostic.config({
+  float = {
+    source = 'always',
+    border = border
+  },
+})
+
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
