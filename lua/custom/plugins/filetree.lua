e1846405f1b55ab03029bd50d7a48a9700ab937c
Jurica Bacurin <jurica@bacurin.de>
2023-05-23 14:55:30 +0200

    update neo-tree config for nerd-font v3

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -15,7 +15,44 @@ return {
   },
   config = function ()
     require('neo-tree').setup {
+      default_component_configs = {
+        icon = {
+          folder_empty = "󰜌",
+          folder_empty_open = "󰜌",
+        },
+        git_status = {
+          symbols = {
+            renamed   = "󰁕",
+            unstaged  = "󰄱",
+          },
+        },
+      },
+      document_symbols = {
+        kinds = {
+          File = { icon = "󰈙", hl = "Tag" },
+          Namespace = { icon = "󰌗", hl = "Include" },
+          Package = { icon = "󰏖", hl = "Label" },
+          Class = { icon = "󰌗", hl = "Include" },
+          Property = { icon = "󰆧", hl = "@property" },
+          Enum = { icon = "󰒻", hl = "@number" },
+          Function = { icon = "󰊕", hl = "Function" },
+          String = { icon = "󰀬", hl = "String" },
+          Number = { icon = "󰎠", hl = "Number" },
+          Array = { icon = "󰅪", hl = "Type" },
+          Object = { icon = "󰅩", hl = "Type" },
+          Key = { icon = "󰌋", hl = "" },
+          Struct = { icon = "󰌗", hl = "Type" },
+          Operator = { icon = "󰆕", hl = "Operator" },
+          TypeParameter = { icon = "󰊄", hl = "Type" },
+          StaticMethod = { icon = '󰠄 ', hl = 'Function' },
+        }
+      },
       source_selector = {
+        sources = {
+          { source = "filesystem", display_name = " 󰉓 Files " },
+          { source = "buffers", display_name = " 󰈙 Buffers " },
+          { source = "git_status", display_name = " 󰊢 Git " },
+        },
         winbar = true,
         statusline = false
       }
