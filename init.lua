51742b402054e579310e6296b1e78fc0bb79d382
Jurica Bacurin <jurica@bacurin.de>
2023-10-05 23:48:23 +0200

    improve config

--- init.lua
+++ init.lua
@@ -589,9 +589,7 @@ cmp.setup {
   },
 }
 
--- vim.cmd('Neotree float')
+vim.cmd('Oil')
 
 vim.o.cursorline = true
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
