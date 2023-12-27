267ab150e84b7bc262ef867c5d9cb249b7adea29
Jurica Bacurin <jurica@bacurin.de>
2023-12-27 20:07:41 +0100

    improve oil setup

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,20 +1,30 @@
-vim.keymap.set('n', '<leader>tt', function ()
-  vim.api.nvim_command("Oil")
-end, { desc = '[T]oggle Filetree' })
-
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function ()
-    require("oil").setup({
-      keymaps = {
-        ["<leader>v"] = "actions.select_vsplit",
-        ["<leader>s"] = "actions.select_split",
-        ["<leader>p"] = "actions.preview",
-        ["<leader>r"] = "actions.refresh",
-        ["<leader>t"] = "actions.select_tab",
-      }
-    })
-  end
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("oil").setup({
+            keymaps = {
+                ["<leader>v"] = "actions.select_vsplit",
+                ["<leader>s"] = "actions.select_split",
+                ["<leader>p"] = "actions.preview",
+                ["<leader>r"] = "actions.refresh",
+                ["<leader>t"] = "actions.select_tab",
+            }
+        })
+        vim.keymap.set('n', '<leader>tt', function()
+            vim.api.nvim_command("Oil")
+        end, { desc = '[T]oggle Filetree' })
+
+        local noFile = true
+        for _, buffer in ipairs(vim.fn.getbufinfo()) do
+            if string.len(buffer.name) ~= 0 then
+                noFile = false
+                break
+            end
+        end
+        if noFile then
+            vim.api.nvim_command('Oil')
+        end
+    end
 }
