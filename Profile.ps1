e228f8bb1ee7712aee616c15d968debf59a9750a
Jurica Bacurin <jurica@bacurin.de>
2023-12-28 20:22:25 +0100

    add theme switcher for windows

--- Profile.ps1
+++ Profile.ps1
@@ -1,3 +1,12 @@
 $env:PATH = "C:\Users\juric\AppData\Local\w64devkit\bin;" + $env:PATH
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+function switch_theme {
+    param (
+        [string]$Argument
+    )
+
+    sed -E -i "s/config\.color_scheme = '([^']+)'/config.color_scheme = '$Argument'/" C:\Repos\dotfiles\wezterm\wezterm.lua
+    Write-Output "switched to: $Argument, please reload wezterm config using Shift-Ctrl-r"
+}
