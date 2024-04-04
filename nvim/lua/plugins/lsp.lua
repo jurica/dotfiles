026b0537c2c61814b537aa5eeea56548eccad908
Jurica Bacurin <jurica@bacurin.de>
2024-04-04 21:32:02 +0200

    qmlls config and minor adjustment for nvim nightly

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -97,5 +97,21 @@ return {
                 }
             end,
         }
-    end
+
+    --     local lspconfig = require 'lspconfig'
+    --     local configs = require 'lspconfig.configs'
+    --     if not configs.qml6_lsp then
+    --       configs.qml6_lsp = {
+    --         default_config = {
+    --           cmd = {'/home/jb/Qt/6.7.0/gcc_64/bin/qmlls'},
+    --           filetypes = {'qml'},
+    --           root_dir = function(fname)
+    --             return lspconfig.util.find_git_ancestor(fname)
+    --           end,
+    --           settings = {},
+    --         },
+    --       }
+    --     end
+    --     lspconfig.qml6_lsp.setup{}
+    -- end
 }
