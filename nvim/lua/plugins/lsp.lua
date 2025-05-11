a68665b5b060771af1b76ebf5ae804fe1d614ddc
Jurica Bacurin <jurica@bacurin.de>
2025-05-11 14:42:16 +0200

    fix config after update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -19,46 +19,48 @@ return {
             },
         })
 
-        local on_attach = function(_, bufnr)
-            local nmap = function(keys, func, desc)
-                if desc then
-                    desc = 'LSP: ' .. desc
-                end
-                vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-            end
-            local is_clangd = function()
-                for _, client in pairs(vim.lsp.get_active_clients { bufnr = bufnr }) do
-                    if client.name == "clangd" then
-                        return true
+        vim.api.nvim_create_autocmd('LspAttach', {
+            callback = function(event)
+                local nmap = function(keys, func, desc)
+                    if desc then
+                        desc = 'LSP: ' .. desc
                     end
-                    return false
+                    vim.keymap.set('n', keys, func, { buffer = event.buf, desc = desc })
                 end
-            end
-            if is_clangd() then
-                local sw = require('lspconfig.configs.clangd').commands.ClangdSwitchSourceHeader[1]
-                nmap('<F4>', sw, 'F4 - switch source/header')
-            end
-            nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-            nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-            nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-            nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-            nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-            nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-            nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-            nmap('<leader>Ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-            nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-            nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-            nmap('<leader>Wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-            nmap('<leader>Wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-            nmap('<leader>Wl', function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end, '[W]orkspace [L]ist Folders')
+                local is_clangd = function()
+                    for _, client in pairs(vim.lsp.get_active_clients { bufnr = event.buf }) do
+                        if client.name == "clangd" then
+                            return true
+                        end
+                        return false
+                    end
+                end
+                if is_clangd() then
+                    local sw = require('lspconfig.configs.clangd').commands.ClangdSwitchSourceHeader[1]
+                    nmap('<F4>', sw, 'F4 - switch source/header')
+                end
+                nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+                nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+                nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+                nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+                nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+                nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+                nmap('<leader>Ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+                nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+                nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+                nmap('<leader>Wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+                nmap('<leader>Wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+                nmap('<leader>Wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, '[W]orkspace [L]ist Folders')
 
-            vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-                vim.lsp.buf.format()
-            end, { desc = 'Format current buffer with LSP' })
-            nmap('<leader>df', vim.lsp.buf.format, 'LSP: format document')
-        end
+                vim.api.nvim_buf_create_user_command(event.buf, 'Format', function(_)
+                    vim.lsp.buf.format()
+                end, { desc = 'Format current buffer with LSP' })
+                nmap('<leader>df', vim.lsp.buf.format, 'LSP: format document')
+            end,
+        })
 
         local servers = {
             clangd = {
@@ -90,20 +92,6 @@ return {
             root_dir = require("lspconfig.util").find_git_ancestor,
             single_file_support = true,
         })
-
-        mason_lspconfig.setup_handlers {
-            function(server_name)
-                require('lspconfig')[server_name].setup {
-                    capabilities = capabilities,
-                    on_attach = on_attach,
-                    settings = servers[server_name],
-                    handlers = {
-                        ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {border = 'single'}),
-                        ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {border = 'single'})
-                    },
-                }
-            end,
-        }
 
     --     local lspconfig = require 'lspconfig'
     --     local configs = require 'lspconfig.configs'
--- nvim/lua/plugins/smart-splits.lua
+++ nvim/lua/plugins/smart-splits.lua
@@ -2,7 +2,6 @@
 return {
   'mrjones2014/smart-splits.nvim',
   config = function ()
-    vim.keymap.set('n', '<c-w>r', require('smart-splits').start_resize_mode, { desc = '[h] Enter resize mode'})
     vim.keymap.set('n', '<M-h>', require('smart-splits').move_cursor_left, { desc = 'Move focus left'})
     vim.keymap.set('n', '<M-j>', require('smart-splits').move_cursor_down, { desc = 'Move focus down'})
     vim.keymap.set('n', '<M-k>', require('smart-splits').move_cursor_up, { desc = 'Move focus up'})
@@ -11,32 +10,5 @@ return {
     vim.keymap.set('n', '<C-j>', require('smart-splits').resize_down, { desc = 'Resize down'})
     vim.keymap.set('n', '<C-k>', require('smart-splits').resize_up, { desc = 'Resize up'})
     vim.keymap.set('n', '<C-l>', require('smart-splits').resize_right, { desc = 'Resize right'})
-    require('smart-splits').setup({
-      resize_mode = {
-        silent = true,
-        hooks = {
-          on_enter = function ()
-            require('lualine').setup({
-              sections = {lualine_a = {{
-                function ()
-                  return 'RESIZE'
-                end
-              }}},
-              winbar = {lualine_a = {{
-                function ()
-                  return 'RESIZE'
-                end
-              }}}
-            })
-          end,
-          on_leave = function ()
-            require('lualine').setup({
-              sections = {lualine_a = {'mode'}},
-              winbar = {lualine_a = {'mode'}}
-            })
-          end
-        }
-      }
-    })
   end
 }
